dfc57e6496eae67829de345c80d4abf1b9af7533
budsalvetti <budsalvetti@gmail.com>
2022-11-26 17:06:40 -0800

    1 file modified

--- index.ts
+++ index.ts
@@ -1,57 +1,22 @@
 // Import stylesheets
 import './style.css';
 
-class Folder {
+// A Class to represent a Folder
+export class Folder {
   constructor(public name: string, public folders: Folder[], public files: FakeFile[]){}
 }
 
-class FakeFile {
+// A Class to represent a File. The name is FakeFile because the term 'File' is a protected keyword
+export class FakeFile {
   constructor(public name: string, public size: number){}
 }
 
-class FileNamePrinter {
-
-public printFileNames(folder: Folder,
-			 recursedFiles?: FakeFile[],
-			 recursedFolders?: Folder[]): string[] {
-
-  let files: FakeFile[] = [];
-  let folders: Folder[] = [];
-  let returnArray: string[];
-
-  if(recursedFiles && recursedFiles.length){
-    files =	files.concat(recursedFiles);
-  }
-
-  if(recursedFolders && recursedFolders.length){
-	  folders = folders.concat(recursedFolders)
-  }
-
-  // get the top level folder file names and push them into the files array
-  if(folder.files.length) {
-    files = files.concat(folder.files);
-  }
-
-  // if there are any child folders will add them to the array to recurse them
-  if(folder.folders.length){
-    folders = folders.concat(folder.folders);
-  }
-
- // now if there are any folders left we will pop off the last element and send back all
- // remaining folders as folders to recurse and recurse the files as well, and keep going till 		 
- //there are no more folders left
-  if(folders.length){
-	  this.printFileNames(folders.pop() , files, folders);	
-  }
-    
-  return  files.map(file => file.name);
-  
-}
-
-}
-
-function printFileNames(){
-
+/**
+ * This function will prepare some mock data which is a
+ * Folder object that contains some child Folders
+ */
+ function getMockFolder() : Folder {
+  // start creation 
   const childFolders: Folder[] = [];
   const childFiles: FakeFile[] = [];
   const childFolder = new Folder('childFolder',[],[]);
@@ -61,13 +26,59 @@ function printFileNames(){
   const topLevelFile = new FakeFile('childFile_1',2);
   childFiles.push(topLevelFile);
 
-  const topLevelFolder = new Folder('topLevel', childFolders, childFiles);
-
-  const fileNamePrinter: FileNamePrinter = new FileNamePrinter();
- 
-  console.log(fileNamePrinter.printFileNames(topLevelFolder));
+  return new Folder('topLevel', childFolders, childFiles);
 }
 
+
+export class FileNameGetter {
+
+  public getFileNames(folder: Folder,
+    recursedFiles?: FakeFile[],
+    recursedFolders?: Folder[]): string[] {
+
+    let files: FakeFile[] = [];
+    let folders: Folder[] = [];
+
+    if (recursedFiles && recursedFiles.length) {
+      files = files.concat(recursedFiles);
+    }
+
+    if (recursedFolders && recursedFolders.length) {
+      folders = folders.concat(recursedFolders)
+    }
+
+    // get the top level folder file names and push them into the files array
+    if (folder.files.length) {
+      files = files.concat(folder.files);
+    }
+
+    // if there are any child folders will add them to the array to recurse them
+    if (folder.folders.length) {
+      folders = folders.concat(folder.folders);
+    }
+
+    /* now if there are any folders left we will pop off the last element and send back all
+     remaining folders as folders to recurse and recurse the files as well, and keep going till 		 
+    there are no more folders left */
+    if (folders.length) {
+      this.getFileNames(folders.pop(), files, folders);
+    }
+
+    // return a new array of the name portion only of the file
+    return files.map(file => file.name).sort();
+
+  }
+
+}
+
+
+// do it!
+function printFileNames(){
+  const fileNameGetter: FileNameGetter = new FileNameGetter();
+  console.log(fileNameGetter.getFileNames(getMockFolder()));
+}
+
+// call the method to kick off execution
 printFileNames();
 
 
